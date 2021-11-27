b993cd7285975dec429dabe0653f126573f75082
amirarabirazdadeh <92179065+amirarabirazdadeh@users.noreply.github.com>
2021-11-27 13:58:27 +0330

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,11 @@
 module.exports = async function(context) {
+    const job = context.request.body.job_id;
+    const jobStatus = context.request.body.job_status;
+
+    // do some db write if required to save the status
+
     return {
         status: 200,
-        body: "Hello, Amir Arabi!!\n"
+        body: "Successfully saved job status for job ID: " + job
     };
 }
